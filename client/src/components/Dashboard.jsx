5899d11c6ecf1ed4701a20b2d85f191a3b79dac0
NickeGilbert <root@Nicklass-MacBook-Pro.local>
2017-07-19 15:38:28 +0200

    Added new components

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -7,12 +7,16 @@ import strings  from './lang_config.jsx';
 import default_lang from './default_lang.jsx';
 import Searchfunction from './Searchfunction.jsx';
 import Header from './Header.jsx';
+import Snackbars from './Snackbars.jsx';
+import TimeMenu from './TimeMenu.jsx';
 
 strings.setLanguage(default_lang.lang);
 
 const Dashboard = () => (
   <div>
     <Searchfunction />
+    <Snackbars />
+    <TimeMenu />
   </div>
 );
 
