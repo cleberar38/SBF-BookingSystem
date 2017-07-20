b15bdb8b7b94d75608c700337f5d171921a3f715
cleberar38 <c_leverdo@hotmail.com>
2017-07-20 08:42:33 +0200

    Calendar

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DatePicker from 'material-ui/DatePicker';
+
+/**
+ * The Date Picker defaults to a portrait dialog. The `mode` property can be set to `landscape`.
+ * You can also disable the Dialog passing `true` to the `disabled` property.
+ */
+const Calendar = () => (
+  <div className="calendar-container">
+    <DatePicker hintText="Landscape Dialog" mode="landscape" />
+  </div>
+);
+
+export default Calendar;
